4e254dda95070dd63f06ae5c2c3a399e86eefe5e
GabrielLAraujo <bfrauches@gmail.com>
2025-04-16 15:07:10 -0300

    Remove o arquivo index.ts, que continha a configuração do servidor, simplificando a estrutura do projeto.

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-import { app } from './src/app'
+import { app } from '../src/app'
 import { createServer, IncomingMessage, ServerResponse } from 'http'
 
 const server = createServer((req: IncomingMessage, res: ServerResponse) => {
